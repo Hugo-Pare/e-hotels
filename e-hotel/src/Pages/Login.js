18dba230c2e8e248a379b8a86cadd6baa467600b
Jayden Bergevin <Jberg114@uottawa.ca>
2023-02-22 21:25:32 -0500

    Added login logic and regex expression for users input

--- e-hotel/src/Pages/Login.js
+++ e-hotel/src/Pages/Login.js
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 function Login() {
   const [showClientLogin, setShowClientLogin] = useState(false);
   const [showEmployeeLogin, setShowEmployeeLogin] = useState(false);
+  const [clientData, setClientData] = useState([]);
+  const [employeeData, setEmployeeData] = useState([]);
   const navigate = useNavigate();
 
   const handleClientLogin = () => {
@@ -14,26 +16,59 @@ function Login() {
   const handleEmployeeLogin = () => {
     setShowClientLogin(false);
     setShowEmployeeLogin(true);
-    
   };
-  
 
+  async function checkClientEmail(email) {
+    fetch(`http://127.0.0.1:5000/clients?email=${email}`)
+      .then(response => response.json())
+      .then(json => {
+        setClientData(json);
+        if (json.length === 0) {
+          alert("S'il vous plait vous enregistrer avant.");
+        } else {
+          navigate('/firstpage/c');
+        }
+      });
+  }
+
+  async function checkEmployeeNumber(employee_num) {
+    fetch(`http://127.0.0.1:5000/employes?id_employe=${employee_num}`)
+      .then(response => response.json())
+      .then(json => {
+        setEmployeeData(json);
+        if (json.length === 0) {
+          alert("Votre comptes n'est pas activer");
+        } else {
+          navigate('/firstpage/e');
+        }
+      });
+  }
 
   const handleClientLoginSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const email = formData.get('email');
-    const name = formData.get('name');
-    navigate('/firstpage/c');
+    const regex_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!regex_email.test(email)) {
+      alert("Le email n'est pas valide");
+      return;
+    }
+    else{
+      checkClientEmail(email);
+    }
     
-  };
+    }
 
   const handleEmployeeLoginSubmit = (event) => {
-      event.preventDefault();
-      const formData = new FormData(event.target);
-      const employee_num = formData.get('employee_num');
-      const name = formData.get('name');
-      navigate('/firstpage/e');
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const employee_num = formData.get('employee_num');
+    const regex_employeenum = /^\d{0,8}$/;
+    if (!regex_employeenum.test(employee_num)) {
+      alert("Le numero d'employer n'est pas valide ");
+      return;
+    }
+    checkEmployeeNumber(employee_num)
   }
   
 
