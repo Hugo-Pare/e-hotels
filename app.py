20e03a6934ab8d6fe12df8bf2d3d08a58d97abd5
Hugo Paré <71098494+Hugo-Pare@users.noreply.github.com>
2023-03-12 14:27:25 -0400

    test

--- app.py
+++ app.py
@@ -94,23 +94,24 @@ def signup_user():
         (email,firstname,lastname,country,province_state,city,street_name,street_num,nas,date_enregistrement,telephone,zip_code,))
         connection.commit()
 
-        client_data = {
-            "email": email,
-            "firstname": firstname,
-            "lastname": lastname,
-            "country": country,
-            "province_state": province_state,
-            "city": city,
-            "street_name": street_name,
-            "street_num": street_num,
-            "zip_code": zip_code,
-            "telephone": telephone,
-            "nas": nas,
-            "date_enregistrement": date_enregistrement
-        }
+        # client_data = {
+        #     "email": email,
+        #     "firstname": firstname,
+        #     "lastname": lastname,
+        #     "country": country,
+        #     "province_state": province_state,
+        #     "city": city,
+        #     "street_name": street_name,
+        #     "street_num": street_num,
+        #     "zip_code": zip_code,
+        #     "telephone": telephone,
+        #     "nas": nas,
+        #     "date_enregistrement": date_enregistrement
+        # }
 
         # returns json data of new user
-        return json.dumps(client_data)
+        # return json.dumps(client_data)
+        return []
 
     except Exception as e:
         print(e)
