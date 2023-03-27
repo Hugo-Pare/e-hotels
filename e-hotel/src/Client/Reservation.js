86dc74d29df4ec88b92062cd1dcb8dd930194c06
[Nico242001] <nthiv077@uottawa.ca>
2023-03-26 20:53:52 -0400

    mes reservations pour client fully finished

--- e-hotel/src/Client/Reservation.js
+++ e-hotel/src/Client/Reservation.js
@@ -23,6 +23,26 @@ const Reservation = () => {
   });
   }
 
+  function handleCancel(event) {
+    const id_reservation = event.target.value;
+    if(window.confirm('Voulez-vous vraiment annuler cette réservation?')){
+      console.log("annuler la réservation " + id_reservation)
+      
+      // patch request
+      fetch(`http://127.0.0.1:5000/reservations/${id_reservation}?canceled=true`, {
+          method: "PATCH"
+      })
+      .then(response => response.json())
+      .then(function(json){
+          console.log(json)
+      })
+      window.location.reload(false);
+  }
+  else{
+      // do nothing
+  }
+  }
+
     return (
       <div>
       <h2>Voici vos réservations qui s'approches</h2>
@@ -30,8 +50,8 @@ const Reservation = () => {
         <table className="room-table">
           <thead>
             <tr key="titles">
-              <th>id reservation</th>
-              <th>chaine</th>
+              <th>Id reservation</th>
+              <th>Chaine</th>
               <th>Adresse</th>
               <th># chambre</th>
               <th>CheckIn</th>
@@ -45,13 +65,16 @@ const Reservation = () => {
               
               <tr key={reservation.id_reservation}>
                 <td>{reservation.id_reservation}</td>
-                <td>nom de chaine</td>
-                <td>Adresse</td>
+                <td>{reservation.nom_chaine}</td>{console.log(reservation.postal_code+".")}
+                <td>{reservation.street_num + ", " + reservation.street_name + ", " + reservation.city + ", " + reservation.postal_code.trim() + ", " + reservation.province_state+ ", " + reservation.country}</td>
                 <td>{reservation.num_chambre}</td>
                 <td>{format(Date.parse(reservation.date_checkin), 'yyyy-MM-dd')}</td>
                 <td>{format(Date.parse(reservation.date_checkout), 'yyyy-MM-dd')}</td>
                 <td>${reservation.frais_total}</td>
                 <td>${reservation.frais_restant}</td>
+                <td>
+                    <button value={reservation.id_reservation} onClick={handleCancel}>Annuler</button>
+                </td>
               </tr>
             ))}
           </tbody>
