18d6d2abf8eebc01fb2932960a878b690537d22e
Jayden Bergevin <Jberg114@uottawa.ca>
2023-03-28 11:01:21 -0400

    Added search by id_location in location existante. Missing a fetch to fetch with email and id. will implement once the fetch is done

--- e-hotel/src/Employee/location_existante.js
+++ e-hotel/src/Employee/location_existante.js
@@ -11,6 +11,7 @@ function Location_existante(){
     const [idHotel, setIdHotel] = useState("")
     const [loaded, setLoaded] = useState(false)
     const [email, setEmail] = useState("")
+    const [id_location, setId_location] = useState("")
 
     useEffect(() => {
         getAllLocations()
@@ -26,26 +27,53 @@ function Location_existante(){
         })
     }
 
-    async function getLocation(email){
-        if(email !== ""){
-            // fetch reservations by email
-            setData([])
-            setLoaded(false)
-
-            fetch(`http://127.0.0.1:5000/locations?email_client=${email}`)
-            .then(response => response.json())
-            .then(function(json){
-                setLoaded(true)
-                setData([json])
-            })
-        }
-        else{
-            // refresh reservations
-            setData([])
-            setLoaded(false)
-            getAllLocations()
-        }
+    function handleIdChange(event){
+        setId_location(event.target.value)
     }
+
+
+    async function getLocation(email, id_location) {
+        if (email && id_location) {
+          // fetch reservations by email and id
+          setData([]);
+          setLoaded(false);
+      
+          fetch(`http://127.0.0.1:5000/reservations/pending/${idHotel}?email_client=${email}&id_reservation=${id_location}`)
+            .then((response) => response.json())
+            .then(function (json) {
+              setLoaded(true);
+              setData([json]);
+            });
+        } else if (email) {
+          // fetch reservations by email
+          setData([]);
+          setLoaded(false);
+      
+          fetch(`http://127.0.0.1:5000/locations?email_client=${email}`)
+            .then((response) => response.json())
+            .then(function (json) {
+              setLoaded(true);
+              setData([json]);
+            });
+        } else if (id_location !== "") {
+          // fetch reservations by id
+          setData([]);
+          setLoaded(false);
+      
+          fetch(`http://127.0.0.1:5000/locations/${id_location}`)
+            .then((response) => response.json())
+            .then(function (json) {
+              setLoaded(true);
+              setData([json]);
+            });
+        } else {
+          // refresh reservations
+          setData([]);
+          setLoaded(false);
+          getAllLocations();
+        }
+      }
+      
 
     function handleEmailChange(event){
         setEmail(event.target.value)
@@ -54,12 +82,14 @@ function Location_existante(){
     function handleSearch(){
         console.log("clicked search")
         console.log("email: "+ email)
-        getLocation(email)
+        console.log("id: "+ id_location )
+        getLocation(email, id_location)
     }
 
     function handleClear(){
         console.log("clicked clear")
         setEmail("")
+        setId_location("")
         setData([])
         setLoaded(false)
         getAllLocations()
@@ -69,7 +99,7 @@ function Location_existante(){
         if (location.frais_restant === '0.00') {
           alert('La chambre a déjà été payée !')
         } else {
-          var amount = parseFloat(prompt('Veuillez entrer le montant à payer :')).toFixed(2)
+          var amount = parseFloat(prompt('Veuillez entrer le montant à payer pour la location # '+location.id_location+' :')).toFixed(2)
           while (Number(amount) > Number(parseFloat(location.frais_restant).toFixed(2))) {
             amount = parseFloat(prompt('Le montant ne peut pas dépassé la valeur restante, entrez à nouveau :')).toFixed(2)
           }
@@ -104,6 +134,8 @@ function Location_existante(){
                     <div className="filter">
                         <label>Email : </label>
                         <input type="email" name="email" onChange={handleEmailChange} value={email}/>
+                        <label>Id Location: </label>
+                        <input type="id" name="id_location" onChange={handleIdChange} value={id_location}/>
                         <button onClick={handleSearch}>Search</button>
                         <button onClick={handleClear}>Clear</button> 
                     </div>
