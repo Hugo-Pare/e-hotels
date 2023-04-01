9e0bf0f5183d5415c6aa3f1ed3d5963e0f971252
Jayden Bergevin <Jberg114@uottawa.ca>
2023-04-01 17:26:05 -0400

    Update edit_hotel_room.js

--- e-hotel/src/Employee/edit_hotel_room.js
+++ e-hotel/src/Employee/edit_hotel_room.js
@@ -201,7 +201,6 @@ function Edit_hotel_room(){
                     <option value="5"> 5 </option>
                     <option value="6"> 6 </option>
                     <option value="7"> 7 </option>
-                    <option value="8"> 8 </option>
                 </select>
             </label>
             <br/>
