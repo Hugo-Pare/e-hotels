ef09c14bcd642838f9f0b1635e9fa6096fa0eb17
[Nico242001] <nthiv077@uottawa.ca>
2023-04-06 21:46:54 -0400

    chaging request

--- e-hotel/src/Employee/location_existante.js
+++ e-hotel/src/Employee/location_existante.js
@@ -48,7 +48,7 @@ function Location_existante(){
         if (email && id_location) {
           // fetch reservations by email and id
           console.log("in email and location");
-          fetch(`http://127.0.0.1:5000/location?email_client=${email}&id_reservation=${id_location}`)
+          fetch(`http://127.0.0.1:5000/location?email_client=${email}&id_location=${id_location}&id_hotel=${id_hotel}`)
             .then((response) => response.json())
             .then(function (json) {
               fixDates(json)
@@ -58,7 +58,7 @@ function Location_existante(){
         } else if (email) {
           // fetch reservations by email
           console.log("in email");
-          fetch(`http://127.0.0.1:5000/locations?email_client=${email}`)
+          fetch(`http://127.0.0.1:5000/locations?email_client=${email}&id_hotel=${id_hotel}`)
             .then((response) => response.json())
             .then(function (json) {
               fixDates(json)
@@ -68,7 +68,7 @@ function Location_existante(){
         } else if (id_location !== "") {
           // fetch reservations by id
           console.log("location");
-          fetch(`http://127.0.0.1:5000/locations/${id_location}`)
+          fetch(`http://127.0.0.1:5000/locations/${id_location}&id_hotel=${id_hotel}`)
             .then((response) => response.json())
             .then(function (json) {
               fixDates(json)
